7c92f9b0c4c6baff22cbc025832eea216a6264fc
pythonking28 <nirajthakur2828@gmail.com>
2024-07-23 16:25:06 +0545

    Socket settings changed

--- src/utils/Socket.js
+++ src/utils/Socket.js
@@ -2,8 +2,11 @@ import { io } from "socket.io-client";
 
 
 const socket = io(process.env.REACT_APP_SOCKET,{
-    autoConnect: true
-})
+    transports: ["websocket"],
+    path: "/socket.io",
+    reconnectionAttempts: 5,
+    reconnectionDelay: 1000,
+  })
 
 
 export default socket;
